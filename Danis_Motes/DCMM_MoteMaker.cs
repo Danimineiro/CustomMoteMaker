8c1adad10dbf8fb46665931bd07cd6d5828c6122
Dani <daniel_plaster@live.de>
2025-07-20 14:27:03 +0200

    Move MakeMoodMoteFor to MoteMaker

--- Danis_Motes/DCMM_MoteMaker.cs
+++ Danis_Motes/DCMM_MoteMaker.cs
@@ -1,5 +1,8 @@
-﻿using RimWorld;
+﻿using Danis_Motes.Settings;
+using RimWorld;
+using UnityEngine;
 using Verse;
+using Verse.AI;
 
 namespace Danis_Motes;
 
@@ -12,6 +15,60 @@ public static class DCMM_MoteMaker
         if (!pawnTicksTillNextMote.TryGetValue(pawn, out int ticksWhenNextAvailable)) return false;
 
         return Find.TickManager.TicksGame < ticksWhenNextAvailable;
+    }
+
+    public static void MakeMoodMoteFor(Pawn pawn)
+    {
+        if (!pawn.CanHaveMotes()) return;
+
+        if (pawn.Downed && pawn.DevelopmentalStage > DevelopmentalStage.Baby)
+        {
+            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.DownedMote);
+            return;
+        }
+
+        if (pawn.MentalStateDef != null)
+        {
+            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.BreakingMote);
+            return;
+        }
+
+        MentalBreaker mentalBreaker = pawn.mindState.mentalBreaker;
+
+        if (mentalBreaker.BreakExtremeIsImminent)
+        {
+            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.BreakingMote);
+            return;
+        }
+
+        if (mentalBreaker.BreakMajorIsImminent)
+        {
+            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.MajorMote);
+            return;
+        }
+
+        if (mentalBreaker.BreakMinorIsImminent)
+        {
+            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.MinorMote);
+            return;
+        }
+
+        int num = Mathf.RoundToInt(Mathf.Lerp(0f, 4f, (mentalBreaker.CurMood - mentalBreaker.BreakThresholdMinor) / (1f - mentalBreaker.BreakThresholdMinor)));
+
+        switch (num)
+        {
+            case 0 or 1:
+                pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.NeutralMote);
+                return;
+
+            case 2 or 3:
+                pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.ContentMote);
+                return;
+
+            case 4:
+                pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.HappyMote);
+                return;
+        }
     }
 
     public static void SpawnAnimatedBubble(this Pawn pawn, ThingDef thingDef, int cooldownTicks = 45)
--- Danis_Motes/HarmonyPatches/DCMM_MoteMakerPatch.cs
+++ Danis_Motes/HarmonyPatches/DCMM_MoteMakerPatch.cs
@@ -1,9 +1,6 @@
-﻿using Danis_Motes.Settings;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RimWorld;
-using UnityEngine;
 using Verse;
-using Verse.AI;
 
 namespace Danis_Motes.HarmonyPatches;
 [HarmonyPatch(typeof(Selector), nameof(Selector.Select))]
@@ -16,65 +13,11 @@ public class DCMM_MoteMakerPatch
         try
         {
             if (obj is not Pawn pawn) return;
-            MakeMoodMoteFor(pawn);
-        } 
+            DCMM_MoteMaker.MakeMoodMoteFor(pawn);
+        }
         catch (Exception ex)
         {
             throw new DCMM_Exception("An error occured whilst trying to spawn a mote.", ex);
-        }
-    }
-
-    public static void MakeMoodMoteFor(Pawn? pawn)
-    {
-        if (!pawn.CanHaveMotes()) return;
-
-        if (pawn.Downed && pawn.DevelopmentalStage > DevelopmentalStage.Baby)
-        {
-            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.DownedMote);
-            return;
-        }
-
-        if (pawn.MentalStateDef != null)
-        {
-            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.BreakingMote);
-            return;
-        }
-
-        MentalBreaker mentalBreaker = pawn.mindState.mentalBreaker;
-
-        if (mentalBreaker.BreakExtremeIsImminent)
-        {
-            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.BreakingMote);
-            return;
-        }
-
-        if (mentalBreaker.BreakMajorIsImminent)
-        {
-            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.MajorMote);
-            return;
-        }
-
-        if (mentalBreaker.BreakMinorIsImminent)
-        {
-            pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.MinorMote);
-            return;
-        }
-
-        int num = Mathf.RoundToInt(Mathf.Lerp(0f, 4f, (mentalBreaker.CurMood - mentalBreaker.BreakThresholdMinor) / (1f - mentalBreaker.BreakThresholdMinor)));
-
-        switch (num)
-        {
-            case 0 or 1:
-                pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.NeutralMote);
-                return;
-
-            case 2 or 3:
-                pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.ContentMote);
-                return;
-
-            case 4:
-                pawn.SpawnAnimatedBubble(DCMM_SetsSettings.SelectedMoteSetDef.HappyMote);
-                return;
         }
     }
 }
